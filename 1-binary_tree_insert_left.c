381d5d4edd8efeeaa2f89a523f7de3a2abaef036
Malik Vance <malik@atlassian.com>
2024-08-13 18:39:35 -0500

    corret asingment

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -17,7 +17,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	}
 
 	/** allocate memory fo the new node*/
-	*new_node = malloc(sizeof(binary_tree_t));
+	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 	{
 		return NULL;
