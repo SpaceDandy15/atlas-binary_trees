d2d27fe2e6097ce60734bbbe4532b50c59d3aacb
Malik Vance <malik@atlassian.com>
2024-08-14 14:23:21 -0500

    needed to move the beginning of my comment down a line

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
-/** binary_tree_is_root - checks if a node
+/**
+ * binary_tree_is_root - checks if a node
  * is the root of the tree
  * @node: pointer to the node to check
  *
- * Return: 1 if the node is a root, otherwise 0.
- * If node is NULL, return 0.
+ * Return: 1 if the node is a root, otherwise 0. if node is NULL, return 0.
 */
 int binary_tree_is_root(const binary_tree_t *node)
 {
