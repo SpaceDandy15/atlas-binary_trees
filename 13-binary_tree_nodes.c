7b10989f24599dc653dc577dcc25808d353f7214
Malik Vance <malik@atlassian.com>
2024-08-14 19:48:51 -0500

    split my return comment to fit into betty

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -5,7 +5,8 @@
  * in a binary tree.
  * @tree: pointer to the root node of the tree to count the number of nodes.
  *
- * Return: the number of nodes with at least 1 child. If tree is NULL, return 0.
+ * Return: the number of nodes with at least 1 child. If tree is NULL,
+ * return 0.
 */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
