d486287eb16800c323e7c49770c386decb994efe
Malik Vance <malik@atlassian.com>
2024-08-14 21:25:04 -0500

    fix betty

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,9 +9,9 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 size_t left_height, right_height;
-if (tree == NULL)
+	if (tree == NULL)
 {
-    return (0);
+return (0);
 }
 
 left_height = binary_tree_height(tree->left);
